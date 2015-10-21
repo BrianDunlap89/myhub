f86e47cdea12c0445e78f21deccf23083992936b
Brian Dunlap <briandunlap589@gmail.com>
2015-10-21 17:26:14 -0400

    Add controller file.

--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -4,5 +4,27 @@ module Myhub
     base_uri "https://api.github.com"
 
     # Your code here too!
+    def initialize
+      @headers = {
+        "Authorization" => "token #{ENV["AUTH_TOKEN"]}",
+        "User-Agent"    => "HTTParty"
+      }
+    end
+
+    def list_issues
+      Github.get("/orgs/TIY-ATL-ROR-2015-Sep/issues",
+                 headers: @headers, query: { state: "all" })
+    end
+
+    def reopen_issue(number)
+      Github.patch("/repos/TIY-ATL-ROR-2015-Sep/assignments/issues/#{number}", 
+                   headers: @headers, body: { state: "open" }.to_json)
+    end
+
+    def close_issue(number)
+      Github.patch("/repos/TIY-ATL-ROR-2015-Sep/assignments/issues/#{number}", 
+                   headers: @headers, body: { state: "closed" }.to_json)
+    end
+
   end
 end
