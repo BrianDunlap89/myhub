713d7e2761675fcdad5dfe3bdb3ba94b83b2ea3f
Brian Dunlap <briandunlap589@gmail.com>
2015-10-21 17:25:58 -0400

    Add server file.

--- lib/myhub.rb
+++ lib/myhub.rb
@@ -1,5 +1,6 @@
 require "sinatra/base"
 require "httparty"
+require "json"
 require "pry"
 
 require "myhub/version"
@@ -9,8 +10,43 @@ module Myhub
   class App < Sinatra::Base
     set :logging, true
 
-    # Your code here ...
+    get "/" do
+      api = Github.new
+
+      issues = api.list_issues
+      @issue_data = []
+      # binding.pry
+      issues.each do |issue|
+        issue = { 
+                  title: issue["title"], 
+                  url: issue["url"],
+                  number: issue["number"],
+                  state: issue["state"],
+                  created: issue["created_at"],
+                  updated: issue["updated_at"]
+                }
+        @issue_data.push(issue)
+        # binding.pry
+      end
+      erb :index, locals: { issues: @issue_data }
+    end
+
+    post "/issue/reopen/:id" do 
+      api = Github.new
+      api.reopen_issue(params["id"].to_i)
+      "Issue has been reopened."
+    end
+
+    post "/issue/close/:id" do 
+      api = Github.new
+      binding.pry
+        api.close_issue(params["id"].to_i)
+        "Issue has been closed."
+    end  
 
     run! if app_file == $0
+
+
   end
 end
+
